aed7a4682dab12279231083fcb40673a5990fb34
ashebo01@college.harvard.edu <ashebo01@college.harvard.edu>
2016-06-10 12:01:03 +0300

    Restructuring style issues

--- health_bot.py
+++ health_bot.py
@@ -1,17 +1,10 @@
 from flask import Flask, request
-from urlparse import urlparse
-import urllib
 import requests
-import json
-import urlparse
-count =0
-hi = "I am a robot"
 senders = []
 rapidpro_value_key = []
 app = Flask(__name__)
 VERIFY_TOKEN = "[REPLACE WITH FACEBOOK VERIFY TOKEN]"
 ACCESS_TOKEN = "[REPLACE WITH FACEBOOK ACCCESS TOKEN]"
-
 app.config.from_object(__name__)
 app.config.from_envvar("HEALTHBOT_SETTINGS", silent=True)
 VERIFY_TOKEN = app.config["VERIFY_TOKEN"]
@@ -19,28 +12,26 @@ ACCESS_TOKEN = app.config["ACCESS_TOKEN"]
 
 
 def reply(user_id):
-    
     value = float(rapidpro_value_key[len(rapidpro_value_key)-1])/100
     float_value = "%0.2f" % value
     key = str(float_value)
-    urll = "http://www.ethioscholarship.com/wp-content/uploads/2016/06/"+ key +".png"
+    path = "http://www.ethioscholarship.com/wp-content/uploads/2016/06/"
+    url = path + key + ".png"
     data = {
         "recipient": {"id": user_id},
-            "message": {"attachment":{"type":"image", "payload":{"url":urll}}}
-    
+        "message": {"attachment": {"type": "image", "payload": {"url": url}}}
     }
-    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    API_url = "https://graph.facebook.com/v2.6/me/messages?access_token="
+    resp = requests.post(API_url + ACCESS_TOKEN, json=data)
     print(resp.content)
 
 
 @app.route('/rapidpro', methods=['POST'])
 def handle_rapidpro_incoming_messages():
-    datas = request.get_data().decode('utf-8')
-    length= len(eval(request.form["values"]))
+    length = len(eval(request.form["values"]))
     val = (eval(request.form["values"])[length-1]["label"])
     rapidpro_value_key.append(val)
-    hey =  urllib.unquote(datas)
-    if (len(senders) >=1):
+    if (len(senders) >= 1):
         reply(senders[0])
     return "ok"
 
@@ -57,7 +48,6 @@ def handle_facebook_verification():
 def handle_facebook_incoming_messages():
     data = request.json
     sender = data['entry'][0]['messaging'][0]['sender']['id']
-    length = len(data['entry'][0]['messaging'])
     senders.append(sender)
     return "ok"
 
@@ -65,4 +55,3 @@ def handle_facebook_incoming_messages():
 if __name__ == '__main__':
     app.run(debug=True)
     app.logger.debug(VERIFY_TOKEN)
-
